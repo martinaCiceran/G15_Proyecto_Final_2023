5052f94f6e4c0a528cbc897b1d7ff8a7a2199c5e
martu <mciceran@pioix.edu.ar>
2023-10-04 13:18:18 -0300

    registro y firebase

--- index.js
+++ index.js
@@ -41,107 +41,107 @@ const {
 } = require("firebase/auth");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB6k56AztBMThTo-2Z3apX8VU8hh8dj9Mg",
-    authDomain: "tetris-proyecto-final.firebaseapp.com",
-    projectId: "tetris-proyecto-final",
-    storageBucket: "tetris-proyecto-final.appspot.com",
-    messagingSenderId: "425385016005",
-    appId: "1:425385016005:web:b3b6a7be809f7429300eac"
-  };
+  apiKey: "AIzaSyB6k56AztBMThTo-2Z3apX8VU8hh8dj9Mg",
+  authDomain: "tetris-proyecto-final.firebaseapp.com",
+  projectId: "tetris-proyecto-final",
+  storageBucket: "tetris-proyecto-final.appspot.com",
+  messagingSenderId: "425385016005",
+  appId: "1:425385016005:web:b3b6a7be809f7429300eac"
+};
   
-  const appFirebase = initializeApp(firebaseConfig);
-  const auth = getAuth(appFirebase);
+const appFirebase = initializeApp(firebaseConfig);
+const auth = getAuth(appFirebase);
 
 
 
 
 app.get('/', function(req, res)
 {
-    console.log(req.query);
-    res.render('inicio', null);
+  console.log(req.query);
+  res.render('inicio', null);
 });
 
 app.get('/tetris', function(req, res)
 {
-    console.log(req.query);
-    res.render('tetris', null);
+  console.log(req.query);
+  res.render('tetris', null);
 });
 
 app.get('/gameOver', function(req, res)
 {
-    console.log("Soy un pedido GET /gameOver", req.query);
-    res.render('gameOver', null);
+  console.log("Soy un pedido GET /gameOver", req.query);
+  res.render('gameOver', null);
 });
 
 app.get("/registro", (req, res) => {
-    res.render("registro");
-  });
+  res.render("registro");
+});
   
-  app.post("/enviarRegistro", async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      await authService.registerUser(auth, { email, password });
-      res.render("registro", {
-        message: "Registro exitoso. Puedes iniciar sesión ahora.",
-      });
-    } catch (error) {
-      console.error("Error en el registro:", error);
-      res.render("registro", {
-        message: "Error en el registro: " + error.message,
-      });
-    }
-  });
-  
-  app.get("/login", (req, res) => {
-    res.render("login");
-  });
-  
-  app.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      const userCredential = await authService.loginUser(auth, {
-        email,
-        password,
-      });
-      // Aquí puedes redirigir al usuario a la página que desees después del inicio de sesión exitoso
-      res.redirect("/inicio");
-    } catch (error) {
-      console.error("Error en el inicio de sesión:", error);
-      res.render("login", {
-        message: "Error en el inicio de sesión: " + error.message,
-      });
-    }
-  });
+app.post("/enviarRegistro", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    await authService.registerUser(auth, { email, password });
+    res.render("login", {
+      message: "Registro exitoso. Puedes iniciar sesión ahora.",
+    });
+  } catch (error) {
+    console.error("Error en el registro:", error);
+    res.render("registro", {
+      message: "Error en el registro: " + error.message,
+    });
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userCredential = await authService.loginUser(auth, {
+      email,
+      password,
+    });
+    // Aquí puedes redirigir al usuario a la página que desees después del inicio de sesión exitoso
+    res.redirect("/inicio");
+  } catch (error) {
+    console.error("Error en el inicio de sesión:", error);
+    res.render("login", {
+      message: "Error en el inicio de sesión: " + error.message,
+    });
+  }
+});
 
 app.get('/admin', async function(req, res)
 {
-    console.log("Soy un pedido GET /iraadmin", req.query);
-    // let preguntas = await MySQL.realizarQuery("SELECT id_pregunta FROM Preguntas;");
-    //console.log(preguntas)
-    // console.log(preguntas[1].id_pregunta)
-    res.render('administrador', null/*{preguntas: preguntas}*/);
+  console.log("Soy un pedido GET /iraadmin", req.query);
+  // let preguntas = await MySQL.realizarQuery("SELECT id_pregunta FROM Preguntas;");
+  //console.log(preguntas)
+  // console.log(preguntas[1].id_pregunta)
+  res.render('administrador', null/*{preguntas: preguntas}*/);
 });
 
 
 app.get('/logout', function(req, res)
 {
-    console.log("Soy un pedido GET", req.query);
-    res.render('inicio', null);
+  console.log("Soy un pedido GET", req.query);
+  res.render('inicio', null);
 });
 
 
 app.get('/home-admin', function(req, res)
 {
-    console.log("Soy un pedido GET", req.query);
-    res.render('home-admin', null);
+  console.log("Soy un pedido GET", req.query);
+  res.render('home-admin', null);
 });
 
 
 app.get('/ranking', async function(req, res){
-    console.log("Pedido post /tablaRanking :)")
-    // let usuario_puntaje = await MySQL.realizarQuery('SELECT * FROM Puntaje ORDER BY puntaje DESC')
-    // console.log(usuario_puntaje)
-    res.render('ranking', null/*{puntaje: usuario_puntaje}*/);
+  console.log("Pedido post /tablaRanking :)")
+  // let usuario_puntaje = await MySQL.realizarQuery('SELECT * FROM Puntaje ORDER BY puntaje DESC')
+  // console.log(usuario_puntaje)
+  res.render('ranking', null/*{puntaje: usuario_puntaje}*/);
 })
